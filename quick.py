04f5f5467f309ee6433e09a8595a826e5c6bee6f
Jibril Coulibaly <jibril.coulibaly@gmail.com>
2021-11-15 12:01:55 -0600

    add tangent operator. fix division by zero and issues with mutable numpy array. Put down elements of explicit integration

--- quick.py
+++ quick.py
@@ -22,22 +22,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ################################################################################
-# Material parameters (Table 1 p. 36)
-################################################################################
-muA = 200e6 # Shear modulus of network A, [Pa]
-lamL = 3.25 # Locking stretch, [-]
-kap = 6000e6 # Bulk modulus, [Pa]
-thatA = 3.25e6 # Flow resistance of network A, [Pa]
-a = 0.073 # Pressure dependence of flow, [-]
-mA = mB = 20 # Stress exponential of network A/B, [-]
-muBi = 293e6 # Initial shear modulus of network B, [Pa]
-muBf = 79.1e6 # Final shear modulus of network B, [Pa]
-beta = 31.9 # Evolution rate of muB, [-]
-thatB = 20.1e6 # Flow resistance of network B, [Pa]
-muC = 10.0e6 # Shear modulus of network C, [Pa]
-q = 0.23 #Relatiive contribution of I2 of network C
-
-################################################################################
 # Utility functions and constants
 ################################################################################
 
@@ -91,25 +75,111 @@ def computeStressC(f, mu, k, q, lamL):
     return ((1./(1 + q))*(computeStressAB(f, mu, k, lamL) +
                           q*mu/j*(i1*b - TWOTHIRD*i2*id33 - np.dot(b,b))))
 
+# Compute total stress
+def computeStress(f, feA, feB, muA, muB, muC, k, q, lamL):
+    return (computeStressAB(feA, muA, k, lamL) +
+            computeStressAB(feB, muB, k, lamL) +
+            computeStressC(f, muC, k, q, lamL))
+
 # Compute the velocity gradient of the viscoleastic flow for networks A and B
 # Equations (5), (6) p.35 and equations (7), (8) p.35-36
 # Solve Fvdot = Fe^-1 *gamma*(dev(sigma)/tau)*F. for Fvdot. Left multiply by Fe
 # to solve linear system of the form AX=B instead of computing the inverse:
 # Fe*Fvdot = gamma*(dev(sigma)/tau)*F, to solve for Fv
 # the terms related to temperature are ignored
+# Also return gamma for shear modulus evolution equation (3) p.33
 def computeFvdot(f, fe, sig, that, a, m):
     s = dev(sig)
     p = -ONETHIRD*np.trace(sig)
     t = np.linalg.norm(s)
     g = (t/(that + a*0.5*(p+np.abs(p))))**m
-    return np.linalg.solve(fe, g/t*np.dot(s, f))
+    if (t <= 0):
+        fvdot = np.zeros((3,3))
+    else:
+        fvdot = np.linalg.solve(fe, g/t*np.dot(s, f))
+    return fvdot, g
 
+# Numerical computation of the tangent sitffness matrix
+# Derivative of stress with respect to deformation gradient
+# Time-dependent material -> time-dependent stiffness matrix computed for:
+# - an evolution of the viscoplastic part over 1 timestep
+# - a deformation gradient step consistent with the applied strain rate
+# Store in a 9x9 matrix for simplicity (row-major storage of 3x3 tensors)
+# We could take advantage of the symetry of cauchy stress and store 6x9
+def tangent(sig_t, f_t, fvA_tplusdt, fvB_tplusdt,
+            erate, dt, muA, muB_tplusdt, k, q, lamL):
+    dsig_dF = np.zeros((9,9))
+    for i in range(3):
+        for j in range(3):
+            # F = 1 + H, displacement gradient increment dH=erate*dt
+            f_tplusdt = np.copy(f_t)
+            f_tplusdt[i][j] += erate*dt
+            # Computation of the elastic deformation gradients at t+dt
+            feA_tplusdt = computeFe(f_tplusdt, fvA_tplusdt)
+            feB_tplusdt = computeFe(f_tplusdt, fvB_tplusdt)
+            # Computation of the stress at t+dt
+            sig_tplusdt = computeStress(f_tplusdt, feA_tplusdt, feB_tplusdt,
+                                        muA, muB_tplusdt, muC, k, q, lamL)
+            # Compute element-wise stiffness Finite difference
+            dsig_dF[:,i+j] = (sig_tplusdt - sig_t).flatten()/(erate*dt)
+    return dsig_dF
 
+################################################################################
+# Computation of the isothermal mechanical behavior for a single material point
+################################################################################
 
+# Material parameters (Table 1 p. 36)
+muA = 200e6 # Shear modulus of network A, [Pa]
+lamL = 3.25 # Locking stretch, [-]
+kap = 6000e6 # Bulk modulus, [Pa]
+thatA = 3.25e6 # Flow resistance of network A, [Pa]
+a = 0.073 # Pressure dependence of flow, [-]
+mA = mB = 20 # Stress exponential of network A/B, [-]
+muBi = 293e6 # Initial shear modulus of network B, [Pa]
+muBf = 79.1e6 # Final shear modulus of network B, [Pa]
+beta = 31.9 # Evolution rate of muB, [-]
+thatB = 20.1e6 # Flow resistance of network B, [Pa]
+muC = 10.0e6 # Shear modulus of network C, [Pa]
+q = 0.23 #Relatiive contribution of I2 of network C
 
 # Variables and initial values
 
 
-f = np.diag # Total deformation gradient
-fvA = id3 # Viscoplastic deformation gradient of A
-fvB = id3 # Viscoplastic deformation gradient of B
+sig = np.zeros((3,3)) # Total Cauchy stress
+sigA = np.zeros((3,3)) # Cauchy stress of network A
+sigB = np.zeros((3,3)) # Cauchy stress of network B
+muB = muBi # Shear modulus of network B
+
+f = np.copy(id33) # Total deformation gradient
+feA = np.copy(id33) # Elastic deformation gradient of network A
+fvA = np.copy(id33) # Viscoplastic deformation gradient of network A
+feB = np.copy(id33) # Elastic deformation gradient of network B
+fvB = np.copy(id33) # Viscoplastic deformation gradient of network B
+
+
+# Loading conditions
+erate = 0.002 # Engineering strain rate, [1/s]
+dt = 1 # Timestep (must meet CFL condition), [s]
+
+# For loop of total time to write
+
+# Data saving conditions
+nsave = 1 #, get from SANISAND and replicate
+
+# Explicit time-integration with Forward Euler difference
+# All quantities known at time t
+
+# Computation of viscoplastic deformation gradients at time t+dt
+fvAdot, ga = computeFvdot(f, feA, sigA, thatA, a, mA)
+fvA += fvAdot*dt
+fvB += computeFvdot(f, feB, sigB, thatB, a, mB)[0]*dt
+# Computation of the shear modulus of network B of at time t+dt
+muB += -beta*(muB-muBf)*ga*dt
+
+# Computation of the tangent operator at time t
+ds_df = tangent(sig, f, fvA, fvB, erate, dt, muA, muB, k, q, lamL)
+
+# Determination of the total deformation gradient at time t+dt
+# This calculation is based on the loading conditions and
+# Linearized steps using the tangent stiffness, enforcing loading and boundary
+# conditions using the method of Bardet and Choucair ()
